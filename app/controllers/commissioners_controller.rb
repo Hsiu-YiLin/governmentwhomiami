5820f2db2cddc7f0a3dad676f96f8c53e614a5c9
Hsiu-YiLin <hlin004@fiu.edu>
2016-02-29 04:37:23 -0500

    Added commissioner profile

--- app/controllers/commissioners_controller.rb
+++ app/controllers/commissioners_controller.rb
@@ -3,15 +3,14 @@ class CommissionersController < ApplicationController
 		@commissioner = Commissioner.find_by(district_id: params[:district_id])
 		@commID = @commissioner.district_id
 		@commPicURL = "commissioners/#{@commID}.jpg"
+		@commAboutText = File.read("app/assets/abouttxt/#{@commID}.txt")
+		# IO.binread("#{@commID}.txt")
 	end
 
 	def search
 		user_address=params[:user_address]
-		
 		user_address.inspect
-
 		# Use JS to find commissioner
 		redirect_to commissioners_path(commID)
-	
 	end
 end
