e350396a2bc0408406f07f81a71ff6ee6f8f88cc
Hsiu-Yi Lin <hlin004@fiu.edu>
2016-03-02 14:00:38 -0500

    Changed main form into submit

--- app/assets/javascripts/commissioners.js
+++ app/assets/javascripts/commissioners.js
@@ -1,43 +1,22 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.	
-var addressRequest = "https://gisws.miamidade.gov/ArcGIS/rest/services/MDC_Locators/MD_Locator/GeocodeServer/findAddressCandidates?SingleLine={{ADDRESS}}&f=json&outSR=%7B%22wkid%22%3A2236%7D&outFields=*";
-  
-var commissionerRequest = "https://gisweb.miamidade.gov/ArcGIS/rest/services/CommunityServices/MD_CommunityServices/MapServer/4/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&geometry=%7B%22x%22%3A{{X}}%2C%22y%22%3A{{Y}}%2C%22spatialReference%22%3A%7B%22wkid%22%3A{{wkid}}%2C%22latestWkid%22%3A{{latestWkid}}%7D%7D&geometryType=esriGeometryPoint&inSR={{wkid}}&outFields=ID%2CCOMMNAME&outSR={{wkid}}";
+$(document).on("ready", function() {
+	var addressRequest = "https://gisws.miamidade.gov/ArcGIS/rest/services/MDC_Locators/MD_Locator/GeocodeServer/findAddressCandidates?SingleLine={{ADDRESS}}&f=json&outSR=%7B%22wkid%22%3A2236%7D&outFields=*";
+	  
+	var commissionerRequest = "https://gisweb.miamidade.gov/ArcGIS/rest/services/CommunityServices/MD_CommunityServices/MapServer/4/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&geometry=%7B%22x%22%3A{{X}}%2C%22y%22%3A{{Y}}%2C%22spatialReference%22%3A%7B%22wkid%22%3A{{wkid}}%2C%22latestWkid%22%3A{{latestWkid}}%7D%7D&geometryType=esriGeometryPoint&inSR={{wkid}}&outFields=ID%2CCOMMNAME&outSR={{wkid}}";
 
-var selectedAddressToCall = null;
-var spatialReference = null;
+	var selectedAddressToCall = null;
+	var spatialReference = null;
 
-$(function() {
-	var availableTags = [];
-    var response = null;
-    $( "#find-user-area" ).keyup(function() {
+	$('.js-find-commissioner').on('click', function(event){
+		event.preventDefault();
+		findEverything()
 	});
 
-	$( "#find-commissioner" ).click(function() {
-
-
-    	// Using AJAX to request user address
-    // 	$.ajax({ 
-    // 		type: "POST",
-    // 		url: addressRequest.replace("{{ADDRESS}}", document.getElementById("find-user-area").value,
-    // 		data: response,
-    // 		success: function(data){
-    // 			response = JSON.parse(data);
-    //         	availableTags = [];
-            	
-    //         	console.log(data);
-
-				// if(response.candidates.length > 0) {
-				// 	selectedAddressToCall = response.candidates[0];
-	   //              spatialReference = response.spatialReference;
-	   //          }
-    // 		}
-    // 		error: function(){
-    // 			console.log('Error in user input')
-    // 		},
-    // 		dataType: "json"
-    // 	});
-
+	function findEverything(){
+		console.log(event);
+		var availableTags = [];
+		var response = null;
 
 		$.get( addressRequest.replace("{{ADDRESS}}", document.getElementById("find-user-area").value), function( data ) {
             response = JSON.parse(data);
@@ -47,30 +26,18 @@ $(function() {
 				selectedAddressToCall = response.candidates[0];
                 spatialReference = response.spatialReference;
             }
-           $.get( commissionerRequest.replace("{{X}}", selectedAddressToCall.location.x).replace("{{Y}}", selectedAddressToCall.location.y).replace("{{wkid}}", spatialReference.wkid).replace("{{wkid}}", spatialReference.wkid).replace("{{wkid}}", spatialReference.wkid). replace("{{latestWkid}}", spatialReference.latestWkid)
+   			$.get( commissionerRequest.replace("{{X}}", selectedAddressToCall.location.x).replace("{{Y}}", selectedAddressToCall.location.y).replace("{{wkid}}", spatialReference.wkid).replace("{{wkid}}", spatialReference.wkid).replace("{{wkid}}", spatialReference.wkid). replace("{{latestWkid}}", spatialReference.latestWkid)
                        , function(data) {
-	        var comissioner = JSON.parse(data);
-        
-	       	// alert ("You live in district " + comissioner.features[0].attributes.ID + ". Your comissioner is " +  comissioner.features[0].attributes.COMMNAME)
-	       	commID=comissioner.features[0].attributes.ID;
-	       	pageRedirect(commID); 
-	       	
-    	}); 
-
-        });
-		
-	// console.log(commissionerRequest.replace("{{X}}", selectedAddressToCall.location.x).replace("{{Y}}", selectedAddressToCall.location.y).replace("{{wkid}}", spatialReference.wkid).replace("{{wkid}}", spatialReference.wkid).replace("{{wkid}}", spatialReference.wkid). replace("{{latestWkid}}", spatialReference.latestWkid));
-
-	
-	// var commissionerUrl = `/commissioners/7`;
-
-	// var html = `
-	// 	<a href="${commissionerUrl}">
-	// 		See commissioner's profile
-	// 	</a>`;
-	});
+	        	var comissioner = JSON.parse(data);
+		       	
+	       		commID=comissioner.features[0].attributes.ID;
+	       		pageRedirect(commID); 
+    		});
+	    });
+	};
 
 	function pageRedirect(commissioner_id){
 		window.location="commissioners/"+commissioner_id;
-	}
+	};
 });
+
